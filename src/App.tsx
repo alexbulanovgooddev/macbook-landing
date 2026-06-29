ad68cf9a1a88913772c40bb2a21967baea82ac89
alexbulanovgooddev <alexbulanovgooddev@gmail.com>
2026-06-29 11:49:10 +0300

    feat: добавлена секция производительность со scroll-анимацией

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { AppHeader } from '@/components/AppHeader'
 import { HomeHero } from '@/components/HomeHero'
 import { ProductViewer } from '@/components/ProductViewer'
 import { HomeShowcase } from '@/components/HomeShowcase'
+import { HomePerformance } from '@/components/HomePerformance'
 
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
@@ -16,6 +17,7 @@ function App() {
 				<HomeHero />
 				<ProductViewer />
 				<HomeShowcase />
+				<HomePerformance />
 			</main>
 		</>
 	)
